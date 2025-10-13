895fa4851b61605cc4f8e84768b22625da31053d
AAKUB7 <mdyakub2367@gmail.com>
2025-10-13 20:18:47 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", addTask);
+
+function addTask() {
+  const taskText = input.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("done");
+  });
+
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "❌";
+  delBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    li.remove();
+  });
+
+  li.appendChild(delBtn);
+  taskList.appendChild(li);
+  input.value = "";
+}
